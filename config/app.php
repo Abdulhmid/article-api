f9c395af46177bc53ff533a89a50fd02ad39de91
abdul <abdul.hamid@tlab.co.id>
2016-10-25 05:10:22 +0700

    rev

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Collective\Bus\BusServiceProvider::class,
         Kris\LaravelFormBuilder\FormBuilderServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -237,6 +238,8 @@ return [
         'FormBuilder' => Kris\LaravelFormBuilder\Facades\FormBuilder::class,
         'Uuid' => Webpatser\Uuid\Uuid::class,
         'Input' => Illuminate\Support\Facades\Input::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
